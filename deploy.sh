059e06192c0826c59551a05869933fecab363864
Rigel Lab <rigel.game.lab@gmail.com>
2025-06-07 11:27:25 +0300

    tmp 11

--- deploy.sh
+++ deploy.sh
@@ -1,45 +1,7 @@
-modules=("auth" "logger" "loyalty" "payment" "person" "reservation" "gateway" "web")
-
-all_tests_passed=true
-
-echo "Run all per-service tests"
-for module in "${modules[@]}"; do
-    printf "Testing %s... " "$module"
-
-    if cd "$module"; then
-        output=$(./test.sh 2>&1)
-        if [ $? -eq 0 ]; then
-            printf "ok\n"
-        else
-            echo "$output"
-            printf "failed\n"
-            all_tests_passed=false
-            break
-        fi
-        cd - > /dev/null || exit
-    else
-        echo "Dir not found $module"
-        all_tests_passed=false
-        break
-    fi
-done
-
-if [ "$all_tests_passed" = true ]; then
-    printf "\nAll tests pass. Publishing all\n"
-    for module in "${modules[@]}"; do
-        printf "Publishing %s... " "$module"
-        if cd "$module"; then
-            output=$(./test.sh 2>&1)
-            if [ $? -eq 0 ]; then
-                printf "ok\n"
-            else
-                echo "$output"
-                echo "error!"
-            fi
-            cd - > /dev/null || exit
-        fi
-    done
+./test-and-publish.sh
+if [ $? -eq 0 ]; then
+    printf "tested and published!\n"
+    ./install.sh
 else
-    printf "\nErrors in tests - no publish\n"
-    exit 1
+    echo "Fail deploy!"
 fi
---
+++ test-and-publish.sh
@@ -0,0 +1,56 @@
+modules=("auth" "logger" "loyalty" "payment" "person" "reservation" "gateway" "web")
+
+all_tests_passed=true
+
+echo "Run all per-service tests"
+for module in "${modules[@]}"; do
+    printf "Testing %s... " "$module"
+
+    if cd "$module"; then
+        output=$(./test.sh 2>&1)
+        if [ $? -eq 0 ]; then
+            printf "ok\n"
+        else
+            echo "$output"
+            printf "failed\n"
+            all_tests_passed=false
+            break
+        fi
+        cd - > /dev/null || exit
+    else
+        echo "Dir not found $module"
+        all_tests_passed=false
+        break
+    fi
+done
+
+if [ "$all_tests_passed" = true ]; then
+    printf "\nAll unit tests pass. Test E2E...\n"
+
+    output=$(./e2e.sh 2>&1)
+    if [ $? -eq 0 ]; then
+        printf "ok\n"
+    else
+        echo "$output"
+        printf "failed\nErrors in E2E tests!"
+        exit 1
+    fi
+
+    printf "\nAll tests pass. Publishing all\n"
+    for module in "${modules[@]}"; do
+        printf "Publishing %s... " "$module"
+        if cd "$module"; then
+            output=$(./test.sh 2>&1)
+            if [ $? -eq 0 ]; then
+                printf "ok\n"
+            else
+                echo "$output"
+                echo "error!"
+            fi
+            cd - > /dev/null || exit
+        fi
+    done
+else
+    printf "\nErrors in unit tests - no publish\n"
+    exit 1
+fi
